bb0c7ae2f2bb600a8d11debec9aacbb1770ae14e
Soumil Jaiswal <soumildjaiswal94@gmail.com>
2022-07-15 17:34:45 +0530

    added a search filter functionality
    added search.js
    will be doing the styling of service page later...

---
+++ search.js
@@ -0,0 +1,15 @@
+function filter(){
+    var FilterValue, input, ul, li, i;
+    input = document.getElementById('search');
+    FilterValue = input.value.toUpperCase();
+    ul = document.getElementById('menu');
+    li = document.getElementsByTagName('li');
+    for(i=0; i<li.length; i++){
+        var a = li[i].getElementsByTagName('a')[0];
+        if(a.innerHTML.toUpperCase().indexOf(FilterValue) > -1){
+            li[i].style.display = "";
+        }else{
+            li[i].style.display = "none";
+        }
+    }
+}

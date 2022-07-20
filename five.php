58ede12a9b378d0bf73cc88b3f9b44b0d71db0bb
Shivam Chadokar <93310331+shivamchadokar031@users.noreply.github.com>
2022-07-20 22:15:46 +0530

    Add files via upload

---
+++ five.php
@@ -0,0 +1,33 @@
+<?php
+    require('connection.php');
+    session_start();
+    isset($_POST['name']);
+    $name = stripslashes($_REQUEST['name']);
+    $name = mysqli_real_escape_string($con, $name);
+    isset($_POST['IVRS']);    
+    $ivrs = stripslashes($_REQUEST['IVRS']);
+    $ivrs = mysqli_real_escape_string($con, $ivrs);
+    isset($_POST['mobile']);
+    $Mob = stripslashes($_REQUEST['mobile']);
+    $Mob = mysqli_real_escape_string($con, $Mob);
+    isset($_POST['address']);
+    $address = stripslashes($_REQUEST['address']);
+    $address = mysqli_real_escape_string($con, $address);
+    isset($_POST['complain']);    
+    $complain = stripslashes($_REQUEST['complain']);
+    $complain = mysqli_real_escape_string($con, $complain);
+
+        $query    = "INSERT INTO `complaint` (`Name`, `IVRSNo.`, `Subject`, `Mobile No.`, `Address`, `complain`) VALUES ('$name', '$ivrs', 'Street Light Issue', '$Mob', '$address', '$complain')";
+        if (mysqli_query($con, $query)) {
+            echo '<script>';
+            echo 'alert("Complaint Registered Successfully");';
+            echo 'location = "service.html";';
+            echo '</script>';
+        }else{
+            echo '<script>';
+            echo 'alert("Sorry !! Try Again");';
+            echo 'location = "service.html";';
+            echo '</script>';
+        }
+?>  
+    
